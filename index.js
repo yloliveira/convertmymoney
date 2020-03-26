1ecb8bc8ea9bc7d548209e96f5236816b5f092f6
Yan Oliveira <oliveira.yan02@gmail.com>
2020-03-26 14:48:19 -0300

    add basic home page

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname + '/src', 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.get('/', (req, res) => {
+  res.render('home');
+});
+
 app.listen(3000, error =>
   error ? console.log('error') : console.log('server is running...')
 );
