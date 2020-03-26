765da4cfcaafb4d4ec09bb3eada9caa64fcb0a11
Yan Oliveira <oliveira.yan02@gmail.com>
2020-03-26 14:34:10 -0300

    add entrypoint

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname + '/src', 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(3000, error =>
+  error ? console.log('error') : console.log('server is running...')
+);
